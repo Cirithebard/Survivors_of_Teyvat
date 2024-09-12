ce48bd9c90c26014598388038827e7a7bfb04899
Cirithebard <2890461642@qq.com>
2024-09-12 20:50:11 +0800

    Update 源.cpp
    1.加入了开始界面，制作了按钮逻辑
    2.对动画类进行优化，将加载图片单独列出为atlas类，利用享元模式提升运行效率

--- 源.cpp
+++ 源.cpp
@@ -2,15 +2,22 @@
 #include <string>
 #include <vector>
 
-//É¾³ıÁËÍæ¼ÒËÙ¶È,Íæ¼Ò¸÷ÏîĞÅÏ¢
+//åˆ é™¤äº†ç©å®¶é€Ÿåº¦,ç©å®¶å„é¡¹ä¿¡æ¯
 int idx_current_anim = 0;
 const int PLAYER_ANIM_NUM = 6;
 
-int WINDOW_WIDTH = 1280;
-int WINDOW_HEIGHT = 720;
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+const int BUTTON_WIDTH = 192;
+const int BUTTON_HEIGHT = 75;
+
+
 
 IMAGE img_player_left[PLAYER_ANIM_NUM];
 IMAGE img_player_right[PLAYER_ANIM_NUM];
+
+bool is_game_started = false;
+bool running = 1;
 
 
 #pragma comment(lib,"MSIMG32.LIB")
@@ -24,53 +31,201 @@ inline void putimage_alpha(int x, int y, IMAGE* img)
 	AlphaBlend(GetImageHDC(NULL), x, y, w, h,
 		GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER,0,255,AC_SRC_ALPHA });
 }
-class Animation
+
+class Atlas
 {
 public:
-
-	Animation(LPCTSTR path, int num, int interval)
+	Atlas(LPCTSTR path, int num)
 	{
-		interval_ms = interval;
-
 		TCHAR path_file[256];
 		for (size_t i = 0; i < num; i++)
 		{
 			_stprintf_s(path_file, path, i);
 
 			IMAGE* frame = new IMAGE();
-			loadimage(frame, path_file);//°Ñ×ÊÔ´×¢ÈëÄÚ´æµ±ÖĞ
+			loadimage(frame, path_file);
 			frame_list.push_back(frame);
 		}
 	}
-	~Animation()
+	~Atlas()
 	{
 		for (size_t i = 0; i < frame_list.size(); i++)
 		{
 			delete frame_list[i];
 		}
 	}
+public:
+	std::vector<IMAGE*>frame_list;
+};
+
+Atlas* atlas_player_left;
+Atlas* atlas_player_right;
+Atlas* atlas_enemy_left;
+Atlas* atlas_enemy_right;
+
+class Button
+{
+public:
+	Button(RECT rect,LPCTSTR path_img_idle,LPCTSTR path_img_hovered,LPCTSTR path_img_pushed)
+	{
+		region = rect;
+
+		loadimage(&img_idle, path_img_idle);
+		loadimage(&img_hovered, path_img_hovered);
+		loadimage(&img_pushed, path_img_pushed);
+	}
+
+	~Button() = default;
+
+	void Draw()
+	{
+		switch(status)
+		{
+		case Status::Idle:
+			putimage(region.left, region.top, &img_idle);
+			break;
+		case Status::Hovered:
+			putimage(region.left, region.top, &img_hovered);
+			break;
+		case Status::Pushed:
+			putimage(region.left, region.top, &img_pushed);
+			break;
+		}
+	}
+
+private:
+	enum class Status
+	{
+		Idle = 0,
+		Hovered,
+		Pushed
+	};
+public:
+	void ProcessEvent(const ExMessage& msg)
+	{
+		switch (msg.message)
+		{
+		case WM_MOUSEMOVE:
+			if (status == Status::Idle && CheckCursorHit(msg.x, msg.y))
+				status = Status::Hovered;
+			else if (status == Status::Hovered && !CheckCursorHit(msg.x, msg.y))
+				status = Status::Idle;
+			break;
+		case WM_LBUTTONDOWN:
+			if (CheckCursorHit(msg.x, msg.y))
+				status = Status::Pushed;
+			break;
+		case WM_LBUTTONUP:
+			if (status == Status::Pushed)
+				OnClick();
+			break;
+		default:
+			break;
+		}
+	}
+
+protected:
+	virtual void OnClick() = 0;
+
+private:
+	RECT region;
+	IMAGE img_idle;
+	IMAGE img_hovered;
+	IMAGE img_pushed;
+	Status status = Status::Idle;
+
+private:
+	bool CheckCursorHit(int x, int y)
+	{
+		return x >= region.left && x <= region.right && y >= region.top && y <= region.bottom;
+	}
+
+};
+class Animation
+{
+public:
+
+	//Animation(LPCTSTR path, int num, int interval)
+	//{
+	//	interval_ms = interval;
+
+	//	TCHAR path_file[256];
+	//	for (size_t i = 0; i < num; i++)
+	//	{
+	//		_stprintf_s(path_file, path, i);
+
+	//		IMAGE* frame = new IMAGE();
+	//		loadimage(frame, path_file);//æŠŠèµ„æºæ³¨å…¥å†…å­˜å½“ä¸­
+	//		frame_list.push_back(frame);
+	//	}
+	//}
+	//~Animation()
+	//{
+	//	for (size_t i = 0; i < frame_list.size(); i++)
+	//	{
+	//		delete frame_list[i];
+	//	}
+	//}
+
+	//å…·ä½“æ”¹åŠ¨ä¸ºï¼šåŸæœ¬åŠ è½½å›¾ç‰‡åœ¨Animationç±»åˆ«ä¸­ï¼Œæ”¹åŠ¨ä¹‹åå•ç‹¬å¼€è¾ŸAtlasç±»å®ç°äº«å…ƒæ¨¡å¼ï¼ŒAnimationè°ƒç”¨atlasè¿›è¡Œç¨‹åº
+
+	Animation(Atlas* atlas, int interval)
+	{
+		anim_atlas = atlas;
+		interval_ms = interval;
+	}
+
+	~Animation() = default;
 
 	void Play(int x, int y, int delta)
 	{
 		timer += delta;
 		if (timer >= interval_ms)
 		{
-			idx_frame = (idx_frame + 1) % frame_list.size();
+			idx_frame = (idx_frame + 1) % anim_atlas->frame_list.size();
 			timer = 0;
 		}
 
-		putimage_alpha(x, y, frame_list[idx_frame]);
+		putimage_alpha(x, y, anim_atlas->frame_list[idx_frame]);
 	}
 
 private:
-	int timer = 0;			//¶¯»­¼ÆÊ±Æ÷
-	int idx_frame = 0;		//¶¯»­Ö¡Ë÷Òı
+	int timer = 0;			//åŠ¨ç”»è®¡æ—¶å™¨
+	int idx_frame = 0;		//åŠ¨ç”»å¸§ç´¢å¼•
 	int interval_ms = 0;
-	std::vector<IMAGE*>  frame_list;
+	/*std::vector<IMAGE*>  frame_list;*/
 
+	Atlas* anim_atlas;
 };
 
+class StartGameButton :public Button
+{
+public:
+	StartGameButton(RECT rect, LPCTSTR path_img_idle, LPCTSTR path_img_hovered, LPCTSTR path_img_pushed)
+		:Button(rect, path_img_idle, path_img_hovered, path_img_pushed){};
+	~StartGameButton() = default;
 
+protected:
+	void OnClick()
+	{
+		is_game_started = true;
+
+		mciSendString(_T("play bgm repeat from 0"), NULL, 0, NULL);
+	}
+};
+
+class QuitGameButton :public Button
+{
+public:
+	QuitGameButton(RECT rect, LPCTSTR path_img_idle, LPCTSTR path_img_hovered, LPCTSTR path_img_pushed)
+		:Button(rect,path_img_idle, path_img_hovered, path_img_pushed) {};
+	~QuitGameButton () = default;
+protected:
+	void OnClick()
+	{
+		running = false;
+	}
+};
 class Player
 {
 public:
@@ -78,8 +233,8 @@ public:
 	{
 		loadimage(&img_shadow, _T("img/shadow_player.png"));
 
-		anim_left = new Animation(_T("img/player_left_%d.png"), 6, 45);
-		anim_right = new Animation(_T("img/player_right_%d.png"), 6, 45);
+		anim_left = new Animation(atlas_player_left,45);
+		anim_right = new Animation(atlas_player_right, 45);
 	}
 
 	~Player()
@@ -217,8 +372,8 @@ public:
 	Enemy()
 	{
 		loadimage(&img_shadow, _T("img/shadow_enemy.png"));
-		anim_left = new Animation(_T("img/enemy_left_%d.png"), 6, 45);
-		anim_right = new Animation(_T("img/enemy_right_%d.png"), 6, 45);
+		anim_left = new Animation(atlas_enemy_left, 45);
+		anim_right = new Animation(atlas_enemy_right, 45);
 
 		enum class SpawnEdge
 		{
@@ -228,7 +383,7 @@ public:
 			Right
 		};
 
-		//½«µĞÈË·ÅÖÃÔÚµØÍ¼Íâ±ß½ç´¦µÄËæ»úÎ»ÖÃ
+		//å°†æ•Œäººæ”¾ç½®åœ¨åœ°å›¾å¤–è¾¹ç•Œå¤„çš„éšæœºä½ç½®
 		SpawnEdge edge = (SpawnEdge)(rand() % 4);
 		switch (edge)
 		{
@@ -328,7 +483,7 @@ private:
 };
 
 
-void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)//µĞÈËÉú³Éº¯Êı
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)//æ•Œäººç”Ÿæˆå‡½æ•°
 {
 	const int INTERVAL = 100;
 	static int counter = 0;
@@ -338,9 +493,9 @@ void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)//µĞÈËÉú³Éº¯Êı
 
 void UpdateBullets(std::vector<Bullet>& bullet_list, const Player& player)
 {
-	const double RADIAL_SPEED = 0.0045;				// ¾¶Ïò²¨¶¯ËÙ¶È
-	const double TANGENT_SPEED = 0.0055;			// ÇĞÏò²¨¶¯ËÙ¶È
-	double radian_interval = 2 * 3.14159 / bullet_list.size();//×Óµ¯Ö®¼äµÄ»¡¶È¼ä¸ô£¬×¢ÒâÊÇ»¡¶ÈÖÆ
+	const double RADIAL_SPEED = 0.0045;				// å¾„å‘æ³¢åŠ¨é€Ÿåº¦
+	const double TANGENT_SPEED = 0.0055;			// åˆ‡å‘æ³¢åŠ¨é€Ÿåº¦
+	double radian_interval = 2 * 3.14159 / bullet_list.size();//å­å¼¹ä¹‹é—´çš„å¼§åº¦é—´éš”ï¼Œæ³¨æ„æ˜¯å¼§åº¦åˆ¶
 	POINT player_positon = player.Getposition();
 	double radius = 100 + 25 * sin(GetTickCount() * RADIAL_SPEED);
 	for (size_t i = 0; i < bullet_list.size(); i++)
@@ -355,7 +510,7 @@ void UpdateBullets(std::vector<Bullet>& bullet_list, const Player& player)
 void DrawPlayerScore(int score)
 {
 	static TCHAR text[64];
-	_stprintf_s(text, _T("µ±Ç°Íæ¼ÒµÃ·ÖÎª£º%d"), score);
+	_stprintf_s(text, _T("å½“å‰ç©å®¶å¾—åˆ†ä¸ºï¼š%d"), score);
 
 	setbkmode(TRANSPARENT);
 	settextcolor(RGB(255, 85, 185));
@@ -365,20 +520,43 @@ int main()
 {
 	initgraph(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	mciSendString(_T("open mus/bgm.mp3 alias bgm"), NULL, 0, NULL);		//¸æËßÏµÍ³²¥·ÅÉùÒô	
+	atlas_player_left = new Atlas(_T("img/player_left_%d.png"), 6);
+	atlas_player_right = new Atlas(_T("img/player_right_%d.png"), 6);
+	atlas_enemy_left = new Atlas(_T("img/enemy_left_%d.png"), 6);
+	atlas_enemy_right = new Atlas(_T("img/enemy_right_%d.png"), 6);
+
+	mciSendString(_T("open mus/bgm.mp3 alias bgm"), NULL, 0, NULL);		//å‘Šè¯‰ç³»ç»Ÿæ’­æ”¾å£°éŸ³	
 	mciSendString(_T("open mus/hit.wav alias hit"), NULL, 0, NULL);
 
-	mciSendString(_T("play bgm repeat from 0"), NULL, 0, NULL);
 	
-	bool running = 1;
+	//bool running = 1;
 	int score = 0;
-	ExMessage msg;												//´´½¨Ò»¸ö½ĞmsgµÄExMessage½á¹¹Ìå,¾ÍÊÇÍâ²¿ÊäÈëµÄĞÅÏ¢
+	ExMessage msg;												//åˆ›å»ºä¸€ä¸ªå«msgçš„ExMessageç»“æ„ä½“,å°±æ˜¯å¤–éƒ¨è¾“å…¥çš„ä¿¡æ¯
 
-	IMAGE img_background;										//´´½¨Ò»¸ö½Ğ ±³¾° µÄIMAGEĞÍ±äÁ¿
+	IMAGE img_background;										//åˆ›å»ºä¸€ä¸ªå« èƒŒæ™¯ çš„IMAGEå‹å˜é‡
+	IMAGE img_menu;
 
-	Player player;												//´´½¨Ò»¸öplayer
-	std::vector<Enemy*>enemy_list;								//µĞÈËÁĞ±í
-	std::vector<Bullet>bullet_list(3);							//×Óµ¯ÁĞ±í
+	Player player;												//åˆ›å»ºä¸€ä¸ªplayer
+	std::vector<Enemy*>enemy_list;								//æ•Œäººåˆ—è¡¨
+	std::vector<Bullet>bullet_list(3);							//å­å¼¹åˆ—è¡¨
+	RECT region_btn_start_game;
+	RECT region_btn_quit_game;
+
+	region_btn_start_game.left = (WINDOW_WIDTH - BUTTON_WIDTH) / 2;
+	region_btn_start_game.right = region_btn_start_game.left + BUTTON_WIDTH;
+	region_btn_start_game.top = 430;
+	region_btn_start_game.bottom = region_btn_start_game.top + BUTTON_HEIGHT;
+
+	region_btn_quit_game.left = (WINDOW_WIDTH - BUTTON_WIDTH) / 2;
+	region_btn_quit_game.right = region_btn_quit_game.left + BUTTON_WIDTH;
+	region_btn_quit_game.top = 550;
+	region_btn_quit_game.bottom = region_btn_quit_game.top + BUTTON_HEIGHT;
+
+	StartGameButton btn_start_game = StartGameButton(region_btn_start_game,
+		_T("img/ui_start_idle.png"), _T("img/ui_start_hovered.png"), _T("img/ui_start_pushed.png"));
+	QuitGameButton btn_quit_game = QuitGameButton(region_btn_quit_game,
+		_T("img/ui_quit_idle.png"), _T("img/ui_quit_hovered.png"), _T("img/ui_quit_pushed.png"));
+	loadimage(&img_menu, _T("img/menu.png"));
 
 	loadimage(&img_background, _T("img/background.png"));
 
@@ -391,65 +569,81 @@ int main()
 		DWORD start_time = GetTickCount();
 
 		while (peekmessage(&msg))
-		{
+		{	if(is_game_started)
 			player.ProcessEvent(msg);
-		}
-		player.Move();
-		UpdateBullets(bullet_list, player);
-		TryGenerateEnemy(enemy_list);
-		for (Enemy* enemy : enemy_list)
-			enemy->Move(player);
-
-		//¼ì²âµĞÈËºÍÍæ¼ÒµÄÅö×²
-		for (Enemy* enemy : enemy_list)
+		else
 		{
-			if (enemy->CheckPlayerCollision(player))
-			{
-				static TCHAR text[128];
-				_stprintf_s(text, _T("×îÖÕµÃ·Ö£º%d !\n´Ë´¦ÎªÕ½°Ücg"), score);
-				MessageBox(GetHWnd(),text,  _T("ÓÎÏ·½áÊø"), MB_OK);
-				running = false;
-				break;
-			}
+			btn_start_game.ProcessEvent(msg);
+			btn_quit_game.ProcessEvent(msg);
 		}
-
-		//¼ì²â×Óµ¯ºÍµĞÈËµÄÅö×²
-		for (Enemy* enemy : enemy_list)
+		}
+		if (is_game_started)
 		{
-			for (const Bullet& bullet : bullet_list)
+			player.Move();
+			UpdateBullets(bullet_list, player);
+			TryGenerateEnemy(enemy_list);
+			for (Enemy* enemy : enemy_list)
+				enemy->Move(player);
+
+			//æ£€æµ‹æ•Œäººå’Œç©å®¶çš„ç¢°æ’
+			for (Enemy* enemy : enemy_list)
 			{
-				if (enemy->CheckBulletCollision(bullet))
+				if (enemy->CheckPlayerCollision(player))
 				{
-					mciSendString(_T("play hit from 0"), NULL, 0, NULL);
-					enemy->Hurt();
-					score++;
+					static TCHAR text[128];
+					_stprintf_s(text, _T("æœ€ç»ˆå¾—åˆ†ï¼š%d !\næ­¤å¤„ä¸ºæˆ˜è´¥cg"), score);
+					MessageBox(GetHWnd(), text, _T("æ¸¸æˆç»“æŸ"), MB_OK);
+					running = false;
+					break;
+				}
+			}
+
+			//æ£€æµ‹å­å¼¹å’Œæ•Œäººçš„ç¢°æ’
+			for (Enemy* enemy : enemy_list)
+			{
+				for (const Bullet& bullet : bullet_list)
+				{
+					if (enemy->CheckBulletCollision(bullet))
+					{
+						mciSendString(_T("play hit from 0"), NULL, 0, NULL);
+						enemy->Hurt();
+						score++;
+					}
+				}
+			}
+
+			//ç§»é™¤æ­»äº¡çš„æ•Œäºº
+			for (size_t i = 0; i < enemy_list.size(); i++)
+			{
+				Enemy* enemy = enemy_list[i];
+				if (!enemy->CheckAlive())
+				{
+					std::swap(enemy_list[i], enemy_list.back());
+					enemy_list.pop_back();
+					delete enemy;
 				}
 			}
 		}
-
-		//ÒÆ³ıËÀÍöµÄµĞÈË
-		for (size_t i = 0; i < enemy_list.size(); i++)
-		{
-			Enemy* enemy = enemy_list[i];
-			if (!enemy->CheckAlive())
-			{
-				std::swap(enemy_list[i], enemy_list.back());
-				enemy_list.pop_back();
-				delete enemy;
-			}
-		}
 		cleardevice();
-		putimage(0, 0, &img_background);
-		player.Draw(1000 / 144);
-		for (Enemy* enemy : enemy_list)
-			enemy->Draw(1000 / 144);
-		for (const Bullet& bullet : bullet_list)
-			bullet.Draw();
-		DrawPlayerScore(score);
-
+		if (is_game_started)
+		{
+			putimage(0, 0, &img_background);
+			player.Draw(1000 / 144);
+			for (Enemy* enemy : enemy_list)
+				enemy->Draw(1000 / 144);
+			for (const Bullet& bullet : bullet_list)
+				bullet.Draw();
+			DrawPlayerScore(score);
+		}
+		else
+		{
+			putimage(0, 0, &img_menu);
+			btn_start_game.Draw();
+			btn_quit_game.Draw();
+		}
 		FlushBatchDraw();
 
-		//±£Ö¤ÊÇ144Ö¡Êı
+		//ä¿è¯æ˜¯144å¸§æ•°
 		DWORD end_time = GetTickCount();
 		DWORD delta_time = end_time - start_time;
 		if (delta_time < 1000 / 144)
@@ -457,6 +651,12 @@ int main()
 			Sleep(1000 / 144 - delta_time);
 		}
 	}
+
+	delete atlas_player_left;
+	delete atlas_player_right;
+	delete atlas_enemy_left;
+	delete atlas_enemy_right;
+
 	EndBatchDraw();
 	return 0;
 }
